6072aa764821f54b086471c3ec0382ffa20749fc
Israel Vieira <vieraisrael02@gmail.com>
2024-11-06 20:40:44 -0300

    Tela de edição de pet

--- lib/pages/pet_edit_page.dart
+++ lib/pages/pet_edit_page.dart
@@ -8,10 +8,118 @@ class PetEdit extends StatefulWidget {
 }
 
 class _PetEditState extends State<PetEdit> {
+  String? selectedRacas;
+
+  final List<String> Raca = ['Caramelo','Buldogue','Husky',];
+  
+  String? selectedGeneros;
+
+  final List<String> Genero = ['Macho','Femea',];
+  
+  String? selectedPedigrees;
+
+  final List<String> Pedigree = ['Sim','Não',];
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: Colors.red),
+    return Scaffold(
+      appBar:AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Editar Informações do Pet'),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: 
+        BoxDecoration(color: Theme.of(context).colorScheme.inversePrimary),
+        child: SingleChildScrollView(
+          padding:EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText:'Nome' ),
+                keyboardType: TextInputType.text,
+              ),
+              DropdownButtonFormField(
+                decoration: InputDecoration(labelText:'Raça' ),
+                value: selectedRacas,
+                items: Raca.map ((Raca){  
+                  return DropdownMenuItem(
+                    value: Raca, 
+                    child: Text(Raca),
+                    );
+                }).toList(),
+                onChanged: (value){
+                  setState((){
+                    selectedRacas = value;
+                  });
+                },
+                
+              ),
+               TextField(
+                decoration: InputDecoration(labelText:'Data de Nascimento' ),
+                keyboardType: TextInputType.datetime,
+              ),
+               TextField(
+                decoration: InputDecoration(labelText:'Peso' ),
+                keyboardType: TextInputType.number,
+              ), 
+              DropdownButtonFormField(
+                decoration: InputDecoration(labelText:'Genero' ),
+                value: selectedGeneros,
+                items: Genero.map ((Genero){  
+                  return DropdownMenuItem(
+                    value: Genero, 
+                    child: Text(Genero),
+                    );
+                }).toList(),
+                onChanged: (value){
+                  setState((){
+                    selectedGeneros = value;
+                  });
+                },
+                
+              ),
+               TextField(
+                decoration: InputDecoration(labelText:'Quantidade de Vacinas' ),
+                keyboardType: TextInputType.number,
+              ), 
+
+               DropdownButtonFormField(
+                decoration: InputDecoration(labelText:'Pedigree?' ),
+                value: selectedPedigrees,
+                items: Pedigree.map ((Pedigree){  
+                  return DropdownMenuItem(
+                    value: Pedigree, 
+                    child: Text(Pedigree),
+                    );
+                }).toList(),
+                onChanged: (value){
+                  setState((){
+                    selectedPedigrees = value;
+                  });
+                },
+                
+              ),
+              const SizedBox(height: 32.0),
+                      Center(
+                        child: ElevatedButton(
+                          onPressed: (){
+                           
+                          },
+                          child: const Text('Salvar Alterações'),
+                        ),
+                      ),
+            ],
+
+          ),
+        ),
+      ),
     );
   }
 }
+
+ 
+
